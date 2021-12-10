6c043b3a7e042e2c8a55f4758a2708e8f64a9f32
fauziahenni <87329906+fauziahenni@users.noreply.github.com>
2021-12-10 06:20:05 -0800

    Add files via upload

---
+++ P5_1_1204013_Host/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace P5_1_1204013_Host
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericTextBox1 = new P5_1_1204013.NumericTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.verticalLabel1 = new P5_1_1204013.VerticalLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Extended  Control : Numeric  TextBox";
+            // 
+            // numericTextBox1
+            // 
+            this.numericTextBox1.Location = new System.Drawing.Point(12, 40);
+            this.numericTextBox1.Name = "numericTextBox1";
+            this.numericTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.numericTextBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Costum Control Label";
+            // 
+            // verticalLabel1
+            // 
+            this.verticalLabel1.Location = new System.Drawing.Point(37, 110);
+            this.verticalLabel1.Name = "verticalLabel1";
+            this.verticalLabel1.Size = new System.Drawing.Size(30, 110);
+            this.verticalLabel1.TabIndex = 3;
+            this.verticalLabel1.Text = "verticalLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.verticalLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Controls Host";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private P5_1_1204013.NumericTextBox numericTextBox1;
+        private System.Windows.Forms.Label label2;
+        private P5_1_1204013.VerticalLabel verticalLabel1;
+    }
+}
+
